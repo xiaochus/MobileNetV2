2700f7f657e6d25071654fbb10e63ca6b8a47e55
xiaochus <xiaochus@live.cn>
2019-05-25 22:04:18 +0800

    ADD: Add alpha parameter(width_multiplier) .

--- mobilenet_v2.py
+++ mobilenet_v2.py
@@ -15,6 +15,16 @@ from keras.utils.vis_utils import plot_model
 from keras import backend as K
 
 
+def _make_divisible(v, divisor, min_value=None):
+    if min_value is None:
+        min_value = divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v
+
+
 def relu6(x):
     """Relu 6
     """
@@ -46,7 +56,7 @@ def _conv_block(inputs, filters, kernel, strides):
     return Activation(relu6)(x)
 
 
-def _bottleneck(inputs, filters, kernel, t, s, r=False):
+def _bottleneck(inputs, filters, kernel, t, alpha, s, r=False):
     """Bottleneck
     This function defines a basic bottleneck structure.
 
@@ -60,6 +70,7 @@ def _bottleneck(inputs, filters, kernel, t, s, r=False):
         s: An integer or tuple/list of 2 integers,specifying the strides
             of the convolution along the width and height.Can be a single
             integer to specify the same value for all spatial dimensions.
+        alpha: Integer, width multiplier.
         r: Boolean, Whether to use the residuals.
 
     # Returns
@@ -67,7 +78,10 @@ def _bottleneck(inputs, filters, kernel, t, s, r=False):
     """
 
     channel_axis = 1 if K.image_data_format() == 'channels_first' else -1
+    # Depth
     tchannel = K.int_shape(inputs)[channel_axis] * t
+    # Width
+    cchannel = int(filters * alpha)
 
     x = _conv_block(inputs, tchannel, (1, 1), (1, 1))
 
@@ -75,7 +89,7 @@ def _bottleneck(inputs, filters, kernel, t, s, r=False):
     x = BatchNormalization(axis=channel_axis)(x)
     x = Activation(relu6)(x)
 
-    x = Conv2D(filters, (1, 1), strides=(1, 1), padding='same')(x)
+    x = Conv2D(cchannel, (1, 1), strides=(1, 1), padding='same')(x)
     x = BatchNormalization(axis=channel_axis)(x)
 
     if r:
@@ -84,7 +98,7 @@ def _bottleneck(inputs, filters, kernel, t, s, r=False):
     return x
 
 
-def _inverted_residual_block(inputs, filters, kernel, t, strides, n):
+def _inverted_residual_block(inputs, filters, kernel, t, alpha, strides, n):
     """Inverted Residual Block
     This function defines a sequence of 1 or more identical layers.
 
@@ -95,23 +109,25 @@ def _inverted_residual_block(inputs, filters, kernel, t, strides, n):
             width and height of the 2D convolution window.
         t: Integer, expansion factor.
             t is always applied to the input size.
+        alpha: Integer, width multiplier.
         s: An integer or tuple/list of 2 integers,specifying the strides
             of the convolution along the width and height.Can be a single
             integer to specify the same value for all spatial dimensions.
         n: Integer, layer repeat times.
+
     # Returns
         Output tensor.
     """
 
-    x = _bottleneck(inputs, filters, kernel, t, strides)
+    x = _bottleneck(inputs, filters, kernel, t, alpha, strides)
 
     for i in range(1, n):
-        x = _bottleneck(x, filters, kernel, t, 1, True)
+        x = _bottleneck(x, filters, kernel, t, alpha, 1, True)
 
     return x
 
 
-def MobileNetv2(input_shape, k):
+def MobileNetv2(input_shape, k, alpha=1.0):
     """MobileNetv2
     This function defines a MobileNetv2 architectures.
 
@@ -119,24 +135,32 @@ def MobileNetv2(input_shape, k):
         input_shape: An integer or tuple/list of 3 integers, shape
             of input tensor.
         k: Integer, number of classes.
+        alpha: Integer, width multiplier, better in [0.35, 0.50, 0.75, 1.0, 1.3, 1.4].
+
     # Returns
         MobileNetv2 model.
     """
-
     inputs = Input(shape=input_shape)
-    x = _conv_block(inputs, 32, (3, 3), strides=(2, 2))
 
-    x = _inverted_residual_block(x, 16, (3, 3), t=1, strides=1, n=1)
-    x = _inverted_residual_block(x, 24, (3, 3), t=6, strides=2, n=2)
-    x = _inverted_residual_block(x, 32, (3, 3), t=6, strides=2, n=3)
-    x = _inverted_residual_block(x, 64, (3, 3), t=6, strides=2, n=4)
-    x = _inverted_residual_block(x, 96, (3, 3), t=6, strides=1, n=3)
-    x = _inverted_residual_block(x, 160, (3, 3), t=6, strides=2, n=3)
-    x = _inverted_residual_block(x, 320, (3, 3), t=6, strides=1, n=1)
+    first_filters = _make_divisible(32 * alpha, 8)
+    x = _conv_block(inputs, first_filters, (3, 3), strides=(2, 2))
 
-    x = _conv_block(x, 1280, (1, 1), strides=(1, 1))
+    x = _inverted_residual_block(x, 16, (3, 3), t=1, alpha=alpha, strides=1, n=1)
+    x = _inverted_residual_block(x, 24, (3, 3), t=6, alpha=alpha, strides=2, n=2)
+    x = _inverted_residual_block(x, 32, (3, 3), t=6, alpha=alpha, strides=2, n=3)
+    x = _inverted_residual_block(x, 64, (3, 3), t=6, alpha=alpha, strides=2, n=4)
+    x = _inverted_residual_block(x, 96, (3, 3), t=6, alpha=alpha, strides=1, n=3)
+    x = _inverted_residual_block(x, 160, (3, 3), t=6, alpha=alpha, strides=2, n=3)
+    x = _inverted_residual_block(x, 320, (3, 3), t=6, alpha=alpha, strides=1, n=1)
+
+    if alpha > 1.0:
+        last_filters = _make_divisible(1280 * alpha, 8)
+    else:
+        last_filters = 1280
+
+    x = _conv_block(x, last_filters, (1, 1), strides=(1, 1))
     x = GlobalAveragePooling2D()(x)
-    x = Reshape((1, 1, 1280))(x)
+    x = Reshape((1, 1, last_filters))(x)
     x = Dropout(0.3, name='Dropout')(x)
     x = Conv2D(k, (1, 1), padding='same')(x)
 
@@ -150,5 +174,5 @@ def MobileNetv2(input_shape, k):
 
 
 if __name__ == '__main__':
-    model = MobileNetv2((224, 224, 3), 100)
+    model = MobileNetv2((224, 224, 3), 100, 1.0)
     print(model.summary())
